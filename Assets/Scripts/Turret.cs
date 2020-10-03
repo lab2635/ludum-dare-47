148e6a3232635b2ceecf1f34f389066dab9f9ab6
Chris Hutchinson <chris@cshutchinson.com>
2020-10-03 00:50:32 -0400

    turret shoots

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private GameObject[] bullets;
+    
+    [SerializeField]
+    public GameObject bulletPrefab;
+    
+    void Start()
+    {
+        bullets = new GameObject[20];
+        
+        for (var i = 0; i < bullets.Length; i++)
+        {
+            bullets[i] = Instantiate(bulletPrefab);
+            bullets[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
