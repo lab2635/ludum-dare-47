4dea799e128e75657b735964959cf636bc1715d0
Chris Hutchinson <chris@cshutchinson.com>
2020-10-03 05:00:33 -0400

    dashing

--- Assets/Scripts/CreatureController.cs
+++ Assets/Scripts/CreatureController.cs
@@ -3,28 +3,28 @@
 [RequireComponent(typeof(CharacterController))]
 public class CreatureController : MonoBehaviour
 {
-    [SerializeField] public float speed = 10;
-    [SerializeField]
+    public float speed = 10;
     public float jumpForce = 5;
-    [SerializeField]
     public Vector3 gravity = new Vector3(0, -10, 0);
-
-    [SerializeField] public Vector3 cameraOffset = new Vector3(0, 10, 0);
-    [SerializeField]
+    public Vector3 cameraOffset = new Vector3(0, 10, 0);
     public float rotationSpeed = 270f;
-
+    public float dashCooldown = 1f;
+    public float dashAcceleration = 200f;
+    
     private Gun gun;
     private Animator animator;
     private CharacterController controller;
     private bool grounded;
     private Vector3 velocity;
     private Vector3 facing;
+    private float dashAccumulator;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         gun = GetComponentInChildren<Gun>();
+        dashAccumulator = dashCooldown;
         AcquireGun();
     }
 
@@ -33,8 +33,21 @@ public class CreatureController : MonoBehaviour
         gun.gunEnabled = true;
     }
 
+    private bool CanDash() => dashAccumulator >= dashCooldown;
+
+    void Dash()
+    {
+        if (CanDash())
+        {
+            velocity += transform.forward * dashAcceleration;
+            dashAccumulator = 0;
+        }
+    }
+    
     void Update()
     {
+        dashAccumulator += Time.deltaTime;
+        
         var input = Vector3.zero;
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
@@ -51,6 +64,11 @@ public class CreatureController : MonoBehaviour
         if (Input.GetButtonDown("Fire1") && gun != null)
         {
             gun.Trigger();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Dash();
         }
         
         if (!controller.isGrounded)
