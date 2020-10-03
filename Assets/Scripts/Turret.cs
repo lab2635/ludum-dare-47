d17389d4ad09fb38ac3a1a847592c316504b8e11
Chris Hutchinson <chris@cshutchinson.com>
2020-10-03 01:58:00 -0400

    turret shoots

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,27 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Doozy.Engine.Nody.Nodes;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    private GameObject[] bullets;
+    private Bullet[] bullets;
+    private float lastFireAccumulator = 0f;
+    private int bulletIndex;
+    private float computedFireRate;
+
+    private const string BulletTag = "Bullet";
     
     [SerializeField]
-    public GameObject bulletPrefab;
+    public float fireRate = 5f;
+    [SerializeField]
+    public Bullet bulletPrefab;
+
+    public void Hit(Bullet bullet, Collider collision)
+    {
+        if (!collision.CompareTag(BulletTag))
+        {
+            Return(bullet);
+        }
+    }
+
+    void Return(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+    }
     
     void Start()
     {
-        bullets = new GameObject[20];
+        bullets = new Bullet[100];
+        computedFireRate = 1 / fireRate;
         
         for (var i = 0; i < bullets.Length; i++)
         {
             bullets[i] = Instantiate(bulletPrefab);
-            bullets[i].SetActive(false);
+            bullets[i].source = this;
+            bullets[i].transform.position = transform.position;
+            bullets[i].transform.rotation = transform.rotation;
+            bullets[i].gameObject.SetActive(false);
         }
+    }
+
+    void Fire(int index = 0)
+    {
+        bullets[index].transform.position = transform.position;
+        bullets[index].transform.rotation = transform.rotation;
+        bullets[index].gameObject.SetActive(true);
+        bulletIndex = (bulletIndex + 1) % bullets.Length;
     }
 
     void Update()
     {
-        
+        lastFireAccumulator += Time.deltaTime;
+
+        if (lastFireAccumulator >= computedFireRate)
+        {
+            lastFireAccumulator = 0;
+            Fire(bulletIndex);
+        }
     }
 }
