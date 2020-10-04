44dc21a61e73503a8697aca348240425b790bbfb
Rein <rein@rein-online.org>
2020-10-04 16:50:04 -0700

    Added sounds for player and lasers.  Death hit by laser and death by time isn't audible for some reason.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,6 +10,41 @@ public class Bullet : MonoBehaviour
 
     public float speed = 11f;
 
+    public Boolean isPlayerBullet;
+    public AudioClip ShootSFX;
+    public AudioClip LaserDeathSFX;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        if (this.audioSource == null)
+        {
+            this.audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        this.audioSource.clip = this.ShootSFX;
+        this.audioSource.playOnAwake = false;
+        this.audioSource.loop = false;
+    }
+
+    private void OnEnable()
+    {
+        if (this.audioSource == null)
+        {
+            this.audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        this.audioSource.clip = this.ShootSFX;
+
+        if (!this.isPlayerBullet)
+        {
+            this.audioSource.spread = 180;
+            this.audioSource.maxDistance = 5;
+            this.audioSource.volume = 0.05f;
+        }
+
+        this.audioSource.Play();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         source.Hit(this, other);
@@ -32,5 +67,12 @@ public class Bullet : MonoBehaviour
         dTemp.detail = 1.0f;
 
         Destroy(exp, 10);
+    }
+
+    public void PlayerImpact()
+    {
+        this.audioSource.clip = this.LaserDeathSFX;
+        this.audioSource.volume = 10f;
+        this.audioSource.Play();
     }
 }
--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
+    public AudioClip AcidDeathSFX;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        this.audioSource = gameObject.AddComponent<AudioSource>();
+        this.audioSource.clip = this.AcidDeathSFX;
+        this.audioSource.playOnAwake = false;
+        this.audioSource.loop = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            this.audioSource.Play();
             GameManager.Instance.KillRespawnPlayer();
         }
     }
