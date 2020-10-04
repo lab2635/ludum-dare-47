89d9242d7d7742c4e37f66f9d186f4ef5233f499
Rein <rein@rein-online.org>
2020-10-03 23:02:33 -0700

    Fixed up the switch interaction and added an animation to it.  Also replaced the button with one that isn't greyboxed.

---
+++ Assets/Scripts/Interactions/CheckpointInteractable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointInteractable : Interactable
+{
+    public bool ActivateCheckpoint;
+    public Checkpoints CheckpointToActivate;
+
+    protected override void OnInteract(ref InteractionEvent ev)
+    {
+        base.OnInteract(ref ev);
+
+        if (this.ActivateCheckpoint)
+        {
+            GameManager.Instance.ActivateCheckpoint(this.CheckpointToActivate);
+        }
+    }
+}
