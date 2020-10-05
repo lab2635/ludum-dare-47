a3b888bfae764522f94d62f858e414d0a9d47015
Chris Hutchinson <chris@cshutchinson.com>
2020-10-05 01:50:23 -0400

    final room; interaction detection fixes; door controller UX

---
+++ Assets/DisplayScreenInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScreenInteraction : Interactable
+{
+    public GameObject screen;
+    
+    protected override void OnInteract(ref InteractionEvent ev)
+    {
+        base.OnInteract(ref ev);
+        screen.SetActive(!screen.activeSelf);
+    }
+}
