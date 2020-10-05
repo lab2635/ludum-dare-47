17ff5fef2b7db4aa47e6bfbd05917479660dba9a
Chris Hutchinson <chris@cshutchinson.com>
2020-10-04 20:07:53 -0400

    room 1 & 2 puzzle wiring

---
+++ Assets/Scripts/Interactions/LeverInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LeverInteraction : ToggleInteraction
+{
+    public LeverController controller;
+
+    public override string description => toggled ? "deactivate" : "activate";
+
+    protected override void OnInteract(ref InteractionEvent ev)
+    {
+        base.OnInteract(ref ev);
+        Toggle(toggled);
+    }
+
+    public void Toggle(bool value)
+    {
+        toggled = value;
+        controller.angle = value ? controller.maxAngle : controller.minAngle;
+    }
+}
+
